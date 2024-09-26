c45596b6368bca9143296d6018b7480cb5a0e4bd
Raunak Verma <mrraunakverma@gmail.com>
2024-09-26 23:44:38 +0530

    Resolved suggested changes and issues

--- oop_student_report_project.py
+++ oop_student_report_project.py
@@ -16,15 +16,38 @@ RollNo. , Blood group, Address and Parents name, emailID.
 
 
 '''
+import time
+"""time is a python module which allows user to work with time by providing functionalities like getting the current time,
+ pausing the Program from executing, etc. """
+
 from prettytable import PrettyTable
 """PrettyTable is a simple Python class library that alows user to easily display 
    tabular data in a visually appealing ASCII table format.
    Documentation link -----> https://pypi.org/project/prettytable/"""
 
 class Student:
-    """The Student class contains multiple attributes related to student details.
-       It also contains a method called add_student() which adds student details and returns the student roll number."""
-    def __init__(self):
+     """
+    Represents a student with various attributes such as name, roll number, marks in different subjects,
+    percentage, blood group, address, and parent information.
+
+    Attributes:
+        name (str): The student's full name.
+        roll_number (str): The student's roll number.
+        m_marks (int): Marks obtained in mathematics.
+        p_marks (int): Marks obtained in physics.
+        c_marks (int): Marks obtained in chemistry.
+        eng_marks (int): Marks obtained in English.
+        percent (float): Overall percentage calculated as the average of subject marks.
+        b_group (str): Blood group of the student.
+        address (str): Address of the student.
+        m_name (str): Mother's name.
+        f_name (str): Father's name.
+        email (str): Email address of the student.
+
+    Methods:
+        add_student(): Allows user input to set the attributes for a student instance.
+    """
+     def __init__(self):
         self.name = None
         self.roll_number = None
         self.m_marks = None
@@ -38,7 +61,12 @@ class Student:
         self.f_name = None
         self.email = None
 
-    def add_student(self):
+
+     def add_student(self):
+        """
+        Prompts the user to input details for a student and calculates the overall percentage.
+        """
+
         self.name = input("\nEnter the student's name: ")
         self.roll_number = input("Enter the student's roll no: ")
         self.m_marks = int(input("Enter the student's maths marks: "))
@@ -51,10 +79,86 @@ class Student:
         self.m_name = input("Enter the student's mother's name: ")
         self.f_name = input("Enter the student's father's name: ")
         self.email = input("Enter the student's email: ")
-        return self.roll_number
 
+def add_info(class_info, student_count):
+    """
+    Add student information to the class details.
 
+    Args:
+    class_details (dict): Dictionary containing student information.
+    num_students (int): Current number of students.
+
+    Returns: (tuple: Updated class_details and num_students.)
+    """
+    student = Student()
+    student.add_student()
+    class_info[student.roll_number] = student
+    student_count += 1
+    return class_info,student_count
+
+def generate_report(total_students, class_data):
+    """
+    Generate a report based on student information.
+
+    Args:
+    num_students (int): Current number of students.
+    class_details (dict): Dictionary containing student information.
+
+    Returns: None
+    """
+    if  total_students == 0:
+        print("No student reports found in this classroom.")
+    else:
+        student_roll_no = input("Enter the roll no. of the student: ")
+        if student_roll_no in class_data:
+            concerned_student = class_data[student_roll_no]
+            report_table = PrettyTable(["Name","Roll no.","Maths","Physics","Chemistry","English","Total Percentage"])
+            report_table.add_row([concerned_student.name,concerned_student.roll_number,concerned_student.m_marks,concerned_student.p_marks,concerned_student.c_marks,concerned_student.eng_marks,concerned_student.percent])
+            print(report_table)
+        else:
+            print("No student data found,please enter a valid roll number.")
+
+def generate_identity(total_students, class_data):
+    """
+    Generate a student's ID.
+    
+    Args:
+    num_students (int): Current number of students.
+    class_details (dict): Dictionary containing student information.
+    
+    Returns: None
+    """
+    if  total_students == 0:
+        print("No student IDs found in this classroom.")
+    else:
+        student_roll_no = input("Enter the roll no. of the student: ")
+        if student_roll_no in class_data:
+            concerned_student = class_data[student_roll_no]
+            id_table = PrettyTable(["Blood Group","Address","Mother's Name","Father's Name","E-mail"])
+            id_table.add_row([concerned_student.b_group,concerned_student.address,concerned_student.m_name,concerned_student.f_name,concerned_student.email])
+            print(id_table)
+        else:
+            print("No student data found,please enter a valid ID.")
+
+def exit_class():
+    """
+    Exit the classroom.
+    Returns: None
+    """
+    time.sleep(2)
+    print("You have exited the classroom.")
+    exit()
+    
 def classroom():
+    """
+    Manage a classroom by allowing user interactions to add student info,
+    generate reports, and view student IDs.
+
+    Usage:
+    (Adding a student's info, Viewing a student's report, Viewing a student's ID, Exiting class)
+
+    Returns: None
+    """
     number_of_students = 0
     class_details = {}
     while True:
@@ -62,38 +166,15 @@ def classroom():
         user_choice = input(f"\nWhat do you want to do?\n1.{options[0]}\n2.{options[1]}\n3.{options[2]}\n4.{options[3]}\n").lower()
 
         if user_choice == "+":
-            student = Student()
-            roll_no = student.add_student()
-            class_details[roll_no] = student
-            number_of_students += 1
+            class_details,number_of_students = add_info(class_details, number_of_students)
+
         elif user_choice == "report":
-            if number_of_students == 0:
-                print("No student reports found in this classroom.")
-            else:
-                student_roll_no = input("Enter the roll no. of the student: ")
-                for head_count in class_details:
-                    if head_count == student_roll_no:
-                        concerned_student = class_details[head_count]
-                        report_table = PrettyTable(["Name","Roll no.","Maths","Physics","Chemistry","English","Total Percentage"])
-                        report_table.add_row([concerned_student.name,concerned_student.roll_number,concerned_student.m_marks,concerned_student.p_marks,concerned_student.c_marks,concerned_student.eng_marks,concerned_student.percent])
-                        print(report_table)
-                    else:
-                        print("No student data found,please enter a valid roll number.")
+            generate_report(number_of_students,class_details)
+
         elif user_choice == "id":
-            if number_of_students == 0:
-                print("No student IDs found in this classroom.")
-            else:
-                student_roll_no = input("Enter the roll no. of the student: ")
-                for head_count in class_details:
-                    if head_count == student_roll_no:
-                        concerned_student = class_details[head_count]
-                        id_table = PrettyTable(["Blood Group","Address","Mother's Name","Father's Name","E-mail"])
-                        id_table.add_row([concerned_student.b_group,concerned_student.address,concerned_student.m_name,concerned_student.f_name,concerned_student.email])
-                        print(id_table)
-                    else:
-                        print("No student data found,please enter a valid ID.")
+            generate_identity(number_of_students,class_details)
         elif user_choice == "exit":
-            exit()
+            exit_class()
 
 if __name__ == '__main__':
-    classroom()
+    classroom() 
